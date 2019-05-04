625c69b6a7208cbac522db2256dfe7ae012dc97d
Alex <alexkarvouniaris@gmail.com>
2019-05-04 21:57:17 +0200

    Project Overview

---
+++ src/project2/ExecutableFile.java
@@ -0,0 +1,22 @@
+package project2;
+
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+public class ExecutableFile extends FitnessFunction{
+	
+	//Fitness function goes here, to DO
+	@Override
+	protected double evaluate(IChromosome arg0) {
+		// TODO Auto-generated method stub
+		double fitness = 0;//here goes the variable that fixes fitness value
+		return fitness;
+	}
+	//Make a configuration object passing the fitness function, to DO
+	
+	//Make a function that fills a disk file with a set of the 4 genes, to DO
+	
+	//Call robocode engine and run a specific battle
+	
+
+}
---
+++ src/project2/SuperTrackerVol2.java
@@ -0,0 +1,10 @@
+package project2;
+
+import robocode.AdvancedRobot;
+
+public class SuperTrackerVol2 extends AdvancedRobot {
+	/*Here goes the updated SuperTracker's code that implements the genetic algorithm.
+	 * 1. Reads the set of the 4 genes(configuration objects) from the disk file
+	 * 2. Creates a population and evolves it
+	 */
+}
