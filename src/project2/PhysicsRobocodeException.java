a5e2d30b0ae1b563032c1e7fd947ccd3547eed5f
nickdndndn <nickd_96@hotmail.com>
2019-05-10 00:02:57 +0200

    Implimented Exception class

---
+++ src/project2/PhysicsRobocodeException.java
@@ -0,0 +1,12 @@
+package project2;
+
+public class PhysicsRobocodeException extends RuntimeException{
+
+	public PhysicsRobocodeException() {
+		super();
+	}
+	
+	public PhysicsRobocodeException(String m) {
+		super(m);
+	}
+}
